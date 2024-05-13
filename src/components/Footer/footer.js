6125e7b1c88602fa0033696c4e8bd93534ece803
olyanya <you@example.com>
2024-05-13 14:29:22 -0400

    feat: added Footer component  & styles

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
+import "./footer.css";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <div className="leftSection">© {currentYear} Olha Horshkova</div>
+      <div className="rightSection">
+        <a href="olga.horshkova@gmail.com" className="footerIcon">
+          <FaEnvelope />
+        </a>
+        <a
+          href="https://github.com/olyanya"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footerIcon"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/olha-horshkova-76a8952a0/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footerIcon"
+        >
+          <FaLinkedin />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
